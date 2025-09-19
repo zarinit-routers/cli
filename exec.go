d017b16b9daa9d58a71c0f8b41a0439dcab07a94
Katy248 <petrovanton247@gmail.com>
2025-09-19 12:13:30 +0300

    All commands now wrapped

--- exec.go
+++ exec.go
@@ -5,30 +5,25 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/charmbracelet/log"
+	l "github.com/charmbracelet/log"
 )
 
-var logger *log.Logger
+var log *l.Logger
 
 func init() {
-	logger = log.Default().WithPrefix("CLI")
+	log = l.Default().WithPrefix("CLI")
 }
 
 func wrapCommand(command string, args ...string) string {
 	return exec.Command(command, args...).String()
 }
 
-func execute(wrapInBash bool, stdin *bytes.Buffer, command string, args ...string) ([]byte, error) {
+func execute(stdin *bytes.Buffer, command string, args ...string) ([]byte, error) {
 
 	var cmd *exec.Cmd
-	if wrapInBash {
-		wrapped := wrapCommand(command, args...)
-		logger.Debugf("Wrapped command to execute `%s`", wrapped)
-		cmd = exec.Command("bash", "-c", wrapped)
-	} else {
-		cmd = exec.Command(command, args...)
-	}
-	logger.Debug("Command", "cmd", cmd.String())
+	wrapped := wrapCommand(command, args...)
+	log.Debug("Wrapped command", "command", wrapped)
+	cmd = exec.Command("bash", "--norc", "-c", wrapped)
 
 	cmd.Env = os.Environ()
 
@@ -44,7 +39,7 @@ func execute(wrapInBash bool, stdin *bytes.Buffer, command string, args ...strin
 
 	err := cmd.Run()
 	if err != nil {
-		logger.Warn("Error while executing command", "command", cmd.String(), "error", err, "stderr", errorBuffer.String())
+		log.Warn("Error while executing command", "command", cmd.String(), "error", err, "stderr", errorBuffer.String())
 		return nil, err
 	}
 	return outputBuffer.Bytes(), err
@@ -57,19 +52,19 @@ func execute(wrapInBash bool, stdin *bytes.Buffer, command string, args ...strin
 // Use simple Execute function instead.
 // But firstly check if it works the same.
 func ExecuteWrap(command string, args ...string) ([]byte, error) {
-	return execute(true, nil, command, args...)
+	return execute(nil, command, args...)
 }
 
 // Run specified command
 func Execute(command string, args ...string) ([]byte, error) {
-	return execute(false, nil, command, args...)
+	return execute(nil, command, args...)
 }
 
 func WithStdin(stdin []byte, command string, args ...string) ([]byte, error) {
-	return execute(false, bytes.NewBuffer(stdin), command, args...)
+	return execute(bytes.NewBuffer(stdin), command, args...)
 }
 
 func ExecuteErr(command string, args ...string) error {
-	_, err := ExecuteWrap(command, args...)
+	_, err := execute(nil, command, args...)
 	return err
 }
